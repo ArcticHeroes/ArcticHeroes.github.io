285b930afe5ec99ac9dd0b1cdfed861f49881467
Nick_Kovtunov <NickKovtunov@users.noreply.github.com>
2024-01-14 18:48:41 +0500

    1 step

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
+import * as React from 'react';
+import { topSteps } from './topSteps';
+import { bottomSteps } from './bottomSteps';
+import CustomStepper from './components/CustomStepper'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+
 function App() {
+  const buttons = [
+    <Button key="one" 
+            onClick={() => {
+              document.getElementById("firstLevel").scrollIntoView();
+            }}>
+      Надземный мир
+    </Button>,
+    <Button key="two" 
+            onClick={() => {
+              document.getElementById("secondLevel").scrollIntoView();
+            }}>
+      Подземный мир
+    </Button>,
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="block1">
+        <div className='block-wrapper'>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className='gameDescription'>
+            От базы в посёлке Уренгой отправляются два отряда арктических героев-изыскателей. 
+            А вокруг окружает бескрайняя снежная лесотундра. 
+            Им предстоит преодолеть безымянную реку, священную сопку ненцев, 
+            подземный мир призрачных древних кочевников сихиртя, гиблое озеро и другие.
+          
+            И всё это для чего? Чтобы достичь Уренгойского вала, огненного дыхания земли.
+          </p>
+          <ButtonGroup size="large" aria-label="large button group">
+            {buttons}
+          </ButtonGroup>
+        </div>
+      </div>
+      <div className='block2' id='firstLevel'>
+        <div className='block-wrapper clearfix'>
+          <Typography variant="h2" gutterBottom>
+            Надземный мир
+          </Typography>
+          <CustomStepper steps={topSteps} />
+        </div>
+      </div>
+      <div className='block3' id='secondLevel'>
+        <div className='block-wrapper'>
+          <Typography variant="h2" gutterBottom>
+            Подземный мир
+          </Typography>
+          <CustomStepper steps={bottomSteps} />
+        </div>
+      </div>
+      <div className='blockFooter'>
+        <p>© Настольня игра "Герои Арктики", 2024</p>
+        <p>Created by <a href="https://vk.com/snezhana_orlova87" target='_blank'>Snezhana Orlova</a></p>
+        <p>Designed and produced by <a href="https://vk.com/nick_kovtunov" target='_blank'>Nick Kovtunov</a></p>
+      </div>
     </div>
   );
 }
---
+++ src/components/CustomStepper.jsx
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stepper from '@mui/material/Stepper';
+import Step from '@mui/material/Step';
+import StepLabel from '@mui/material/StepLabel';
+import StepContent from '@mui/material/StepContent';
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import StepButton from '@mui/material/StepButton';
+
+export default function CustomStepper({ steps }) {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  const handleStep = (step) => () => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 400 }}>
+      <Stepper activeStep={activeStep} orientation="vertical" nonLinear>
+        {steps.map((step, index) => (
+          <Step key={step.id}
+                sx={{
+                  '& .MuiStepLabel-root .css-1u4zpwo-MuiSvgIcon-root-MuiStepIcon-root': {
+                    color: step.color, // circle color (ACTIVE)
+                    width: '1.5em',
+                    height: '1.5em'
+                  },
+                  '& .css-14yr603-MuiStepContent-root': {
+                    marginLeft: '1em'
+                  },
+                }}
+          >
+            {/* <StepLabel
+              optional={
+                <Typography variant="caption">{step.field}</Typography>
+              }
+            >
+              {step.label}
+            </StepLabel> */}
+            <StepButton 
+              color="inherit" 
+              onClick={handleStep(index)}
+              optional={
+                <Typography variant="caption">{step.field}</Typography>
+              }
+            >
+              {step.label}
+            </StepButton>
+            <StepContent>
+              <Typography>{step.description}</Typography>
+              <Box sx={{ mb: 2 }}>
+                <div>
+                  <Button
+                    variant="contained"
+                    onClick={handleNext}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    {index === steps.length - 1 ? 'Закончить уровень' : 'Шаг верёд'}
+                  </Button>
+                  <Button
+                    disabled={index === 0}
+                    onClick={handleBack}
+                    sx={{ mt: 1, mr: 1 }}
+                  >
+                    Шаг назад
+                  </Button>
+                </div>
+              </Box>
+            </StepContent>
+          </Step>
+        ))}
+      </Stepper>
+      {activeStep === steps.length && (
+        <div>
+          <Typography>Уровень окончен</Typography>
+          {/* <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+            Начать сначала
+          </Button> */}
+        </div>
+      )}
+    </Box>
+  )
+}
